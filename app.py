ce52d7d3a4def8f4291a5465866d060afafd9c2a
PotatoY <yjw_0412@naver.com>
2020-01-07 23:59:07 +0900

    feat: Edit app.py to include New Function dict and Finish Making POST
    Add dict function in Item class
    
    Finish making POST

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from secrets import token_urlsafe
 
-from flask import Flask, Response
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,9 +13,9 @@ class Item:
     author: str
     completed: bool
     removed: bool
-    private: bool
     project: str
     target: str
+    private: bool
 
     def __init__(self, title: str, content: str, author: str, project: str = None, target: str = None,
                  private: bool = False):
@@ -29,18 +29,37 @@ class Item:
         self.removed = False
         self.private = private
 
+    @property
+    def dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+            'project': self.project,
+            'target': self.target,
+            'completed': self.completed,
+            'removed': self.removed,
+            'private': self.private}
+
 
 @app.route('/', methods=['GET'])
 def todolist():
     with open('todo.json', 'r') as f:
-        res = Response(json.load(f))
+        res = jsonify(json.load(f))
         res.headers['Content-Type'] = 'application/json; charset=utf-8'
     return res
 
 
 @app.route('/', methods=['POST'])
 def maketodo():
-    pass
+    todo = Item(**request.get_json())
+    with open('todo.json', 'r') as f:
+        todofile = json.load(f)
+        todofile.append(todo.dict)
+    with open("todo.json", "w") as f:
+        json.dump(todofile, f)
+    return todo.dict
 
 
 @app.route('/', methods=['DELETE'])
