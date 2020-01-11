16b75a7b29baedfd9b497588c9018773a7d18ce4
Coder-Iro <doohee006@gmail.com>
2020-01-11 23:11:29 +0900

    Change save way to sqlite from json

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-import json
+import sqlite3
 from flask import Flask, jsonify, request
 from secrets import token_urlsafe
 
 app = Flask(__name__)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+conn = sqlite3.connect("todo.db", isolation_level=None)
+conn.row_factory = dict_factory
+cur = conn.cursor()
+
+
+def load():
+    cur.execute("SELECT * from todo")
+    return cur.fetchall()
 
 
 class Item:
@@ -18,7 +35,7 @@ class Item:
 
     def __init__(self, title: str, content: str, author: str, project: str = None, target: str = None,
                  private: bool = False):
-        self.id = token_urlsafe(10)
+        self.id = token_urlsafe(5)
         self.title = title
         self.content = content
         self.author = author
@@ -39,24 +56,35 @@ class Item:
             'target': self.target,
             'completed': self.completed,
             'removed': self.removed,
-            'private': self.private}
+            'private': self.private,
+        }
+
+    @property
+    def tuple(self):
+        return (
+            self.id,
+            self.title,
+            self.content,
+            self.author,
+            self.project,
+            self.target,
+            self.completed,
+            self.removed,
+            self.private,
+        )
+
 
 @app.route('/', methods=['GET'])
 def todolist():
-    with open('todo.json', 'r') as f:
-        res = jsonify(json.load(f))
-        res.headers['Content-Type'] = 'application/json; charset=utf-8'
+    res = jsonify(load())
+    res.headers['Content-Type'] = 'application/json; charset=utf-8'
     return res
 
 
 @app.route('/', methods=['POST'])
 def maketodo():
     todo = Item(**request.get_json())
-    with open('todo.json', 'r') as f:
-        todofile = json.load(f)
-        todofile.append(todo.dict)
-    with open("todo.json", "w") as f:
-        json.dump(todofile, f)
+    cur.execute("INSERT INTO todo values (?,?,?,?,?,?,?,?,?)", todo.tuple)
     return todo.dict
 
 
